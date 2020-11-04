a2a86256ea41409086597799bb620ad5da7e1271
Kirill-Od <kirill.odeyanenko@gmail.com>
2020-11-04 18:12:06 +0200

    Mobile menu

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
-document.addEventListener("click", function(){
+document.addEventListener("click", function () {
     document.getElementById("site-header__nav-icon").classList.toggle('open');
+    document.getElementById("mobile-menu").classList.toggle('open');
   });
